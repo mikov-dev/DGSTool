a7894350b0d4f9c19ca35662f5722a634fda6b1f
mikov-dev <pnmikov@gmail.com>
2025-01-20 21:33:14 +0200

    initial commit

---
+++ src/main/java/com/pnm/DGSTool/DgsToolApplication.java
@@ -0,0 +1,13 @@
+package com.pnm.DGSTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgsToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgsToolApplication.class, args);
+	}
+
+}
